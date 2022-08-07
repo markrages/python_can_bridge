458d747c103eab8467c95aa52ea5b46a74c168bd
Mark Rages <Mark>
2022-08-07 16:47:33 -0500

    Use the RedirectReader() built into python-can.

--- vcan_bridge.py
+++ vcan_bridge.py
@@ -6,13 +6,19 @@ from time import sleep
 can_bus_a = can.Bus('/dev/ttyCH340', bustype='seeedstudio', bitrate=250000)
 can_bus_b = can.Bus('vcan0', bustype='socketcan')
 
-try:
-    for message in can_bus_a:
-        can_bus_b.send(message)
+listener_a = can.RedirectReader(can_bus_a)
+listener_b = can.RedirectReader(can_bus_b)
 
+notifier_a = can.Notifier(can_bus_a, [listener_b])
+notifier_b = can.Notifier(can_bus_b, [listener_a])
+
+try:
     while True:
         sleep(1)
 
 except KeyboardInterrupt:
     print("\nProgram Ended")
-    exit(0)
+
+finally:
+    notifier_a.stop()
+    notifier_b.stop()
